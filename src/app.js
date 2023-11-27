03c26a3ffa01a853e8012ab04898f4a492bc7e07
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-11-27 09:31:08 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,14 @@ const server = http.createServer((req, res) => {
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
-      
+      if(value1 < 0){
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end("The operation cannot be performed")
+      }
+      else{
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write(`The result is ${value1 ** value2}`)
+      }
     });
     }
 });
